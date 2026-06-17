746cfd738c6a1d04f059db9d2e9dcdb9fdfa2acf
marxcuadros24-png <marxcuadros24@gmail.com>
2026-06-17 10:44:45 -0500

    Fix: Ajuste de ruta para Swagger en /swagger/index.html

--- TiendaMVC/Program.cs
+++ TiendaMVC/Program.cs
@@ -24,24 +24,21 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// 3. CONFIGURACIÓN DEL PIPELINE DE PETICIONES HTTP (MIDDLEWARES)
-
-// Activar Swagger en la Raíz del sitio
+// MODIFICA ESTE BLOQUE DE SWAGGER:
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mi Tienda API V1");
-    c.RoutePrefix = ""; // Al ingresar a la IP limpia, cargará directamente Swagger
+    c.RoutePrefix = "swagger"; // ◄ Cambiado de "" a "swagger"
 });
 
 app.UseCors("Abierta");
 app.UseStaticFiles();
 app.UseRouting();
 
-// Rutas para los Controladores de Vistas convencionales de MVC
+// Ahora la raíz (http://10.142.237.143:8080) volverá a cargar tus vistas de MVC (Productos/Index)
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Productos}/{action=Index}/{id?}");
 
-// 4. ARRANCAR LA APLICACIÓN
 app.Run();
